c4fe67ea852922c8e9bff483a16cd60ed63af66b
josh-programmieren <87899799+josh-programmieren@users.noreply.github.com>
2022-08-02 19:39:37 +0200

    Added button based variant.
    When an user chooses that mode, he is able to press buttons and calculate an expression.

---
+++ button-based.js
@@ -0,0 +1,36 @@
+function getDisplay() {
+    var display = document.getElementById("display");
+    return display;
+}
+
+function clearDisplay() {
+    var display = getDisplay();
+    for (var i = 0; i < display.innerHTML.length; i++) {
+        backspace();
+    }
+    console.log("clear");
+}
+
+function backspace() {
+    var display = getDisplay();
+    var text = display.innerHTML;
+    if (text.length > 0) {
+        display.innerHTML = text.substring(0, text.length - 1);
+    }
+    console.log("backspace");
+}
+
+function add(value) {
+    var display = getDisplay();
+    var text = display.innerHTML;
+    display.innerHTML = text + value;
+    console.log("add " + value);
+}
+
+function calculate() {
+    var display = getDisplay();
+    var text = display.innerHTML;
+    var result = eval(text);
+    display.innerHTML = result;
+    console.log("calculate");
+}
